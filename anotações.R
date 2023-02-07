258f1cad8e6845aa05205540a6f08c678071ad3d
Sarah Ferlin <sarahferlinz@gmail.com>
2023-02-06 21:07:37 -0300

    Adicionar anotações

---
+++ anotações.R
@@ -0,0 +1,6 @@
+# Usamos o Git pra criar um repositorio e Github pra mandar o repositório pro site do Github
+
+
+#  NEW PROJECT - VERSION CONTROL -> COLA O LINK DO GITHHUB NA JANELA DO VERSION CONTROL, DAI VC CONSEGUE MEXER NO R e atualizar direto no github
+
+# FORK - pra criar uma cópia do repositório de alguém e poder contribuir
